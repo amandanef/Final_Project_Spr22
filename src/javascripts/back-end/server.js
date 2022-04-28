e998a7310b4a5a7a94a65ea45e7c662ed20bf36e
Amanda Nef <70547479+amandanef@users.noreply.github.com>
2022-04-28 17:58:14 -0600

    heroku app.express fix

--- src/javascripts/back-end/server.js
+++ src/javascripts/back-end/server.js
@@ -30,7 +30,7 @@ app.use(logger('dev'))
 // Static files
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, '..', '..', '..', 'public')))
+app.use(express.static(__dirname));
 
 // Sessions
 const session = require('express-session')
